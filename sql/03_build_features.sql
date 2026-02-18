82b4060c119a4c94b1c395f24546af5c12afe498
RahulGodara <rahul.godara@indusinsights.com>
2026-02-18 15:48:32 +0530

    change as_of_date variable name

--- sql/03_build_features.sql
+++ sql/03_build_features.sql
@@ -9,7 +9,7 @@
 -- Set "today" for reproducibility. Replace with CURRENT_DATE on RDBMS that supports it.
 WITH params AS (
     SELECT DATE('2025-06-25') AS as_of_date
-    SELECT DATE('2025-07-01') AS as_of_date
+    SELECT DATE('2025-07-01') AS as_of_date2
 ),
 
 txns_30d AS (
